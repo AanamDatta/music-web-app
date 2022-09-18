036586029827a091d733b53b9d15f40040aa6d65
AanamDatta <78989806+AanamDatta@users.noreply.github.com>
2022-09-17 21:59:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup() {
+    canvas = createCanvas(600,500)
+    canvas.position(500,150)
+    video = createCapture(VIDEO)
+    video.hide()
+    }
+    function draw() {
+        image(video,0,0,600,500 )
+    }
